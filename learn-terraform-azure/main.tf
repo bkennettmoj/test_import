658d13300c77ed641940cbdefc4e166c2756e7af
Bill Kennett <bill.kennett@digital.justice.gov.uk>
2020-11-25 17:27:07 +0000

    Successfully imported existing azure group and associated user-member

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -13,3 +13,21 @@
     }
   }
 
+data "azuread_user" "example" {
+   user_principal_name = "TempTestUser2@nomsdigitechoutlook.onmicrosoft.com"
+ #  object_id = "c7588935-12cf-4fd1-96fa-6040e1a5175c"
+  }
+
+data "azuread_group" "example_MyGroup" {
+#    name = "MyGroup"
+    object_id = "37437fc1-e265-48fd-a1ae-4bc0fb127c36"
+  }
+ 
+ resource "azuread_group_member" "MyGroup_TempTestUser2" {
+    group_object_id   = data.azuread_group.example_MyGroup.id
+    member_object_id  = data.azuread_user.example.id
+   lifecycle {
+      create_before_destroy = true
+    }
+  }
+
